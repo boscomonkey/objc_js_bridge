d8ac689f136e33459dfd149646404a09fc71f27f
Bosco So <bso@alum.mit.edu.nospam>
2016-06-10 13:03:20 -0700

    first pass at trapping AJAX calls for Objective-C

---
+++ ajax_trap.js
@@ -0,0 +1,37 @@
+(function() {
+    var s_ajaxListener = new Object();
+    var newLocation = null;
+
+    s_ajaxListener.tempOpen = XMLHttpRequest.prototype.open;
+    s_ajaxListener.tempSend = XMLHttpRequest.prototype.send;
+    s_ajaxListener.callback = function () {
+        newLocation = 'mpajaxhandler://' + this.url;
+
+        console.log("newLocation:", newLocation);
+        window.location.href = newLocation;
+    };
+
+    XMLHttpRequest.prototype.open = function(a,b) {
+        if (!a) { var a=''; }
+        if (!b) { var b=''; }
+        s_ajaxListener.tempOpen.apply(this, arguments);
+        s_ajaxListener.method = a;
+        s_ajaxListener.url = b;
+        if (a.toLowerCase() == 'get') {
+            s_ajaxListener.data = b.split('?');
+            s_ajaxListener.data = s_ajaxListener.data[1];
+        }
+    }
+
+    XMLHttpRequest.prototype.send = function(a,b) {
+        if (!a) { var a=''; }
+        if (!b) { var b=''; }
+        s_ajaxListener.tempSend.apply(this, arguments);
+        if(s_ajaxListener.method.toLowerCase() == 'post') {
+            s_ajaxListener.data = a;
+        }
+        s_ajaxListener.callback();
+    }
+
+    console.log('AJAX calls are now trapped.');
+})()
---
+++ load_script.js
@@ -0,0 +1,7 @@
+(function() {
+
+    var s = document.createElement('script');
+    s.setAttribute('src', 'http://localhost:8000/ajax_trap.js');
+    document.body.appendChild(s);
+
+})()
